326b6380a840cca0d9e0109f727bba75c5a5e7a6
robert <robgladue@hotmail.com>
2021-12-03 01:48:27 +0000

    implemented mocha chai testing to Jason's Cat

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,21 @@
 const request = require("request");
+const input = process.argv.slice(2);
 
-
-
-const fetchBreedDescription = function(breedName, callback)  {
-
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName[0]}`, function(err, res, body) {
-    if (err) {return callback(err)}
-    else{
-    const data = JSON.parse(res.body);
-    callback(data[0].description);return;
+const fetchBreedDescription = function (breedName, callback) {
+  breedName = input[0];
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    function (err, res, body) {
+      if (err) {
+        callback(err, null);
+      } else {
+        const data = JSON.parse(res.body);
+        
+        callback(null, data[0].description);
+        return;
+      }
     }
-  })
+  );
 };
 
-module.exports = {fetchBreedDescription}
+module.exports = { fetchBreedDescription };
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const {fetchBreedDescription} = require('./breedFetcher')
+const { fetchBreedDescription } = require("./breedFetcher");
 const breedName = process.argv.slice(2);
 
 fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
-    return console.log('Error fetch details:', error);
+    return error;
   } else {
     return console.log(desc);
   }
